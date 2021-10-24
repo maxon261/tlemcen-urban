86b360908833500d3a6a497271a5467530a96836
maxon261 <malikoo13000@gmail.com>
2021-10-25 01:19:53 +0200

    change domain

--- wp-login.php
+++ wp-login.php
@@ -5,16 +5,16 @@
 	<title>Se connecter &lsaquo; Urban Tlemcen &#8212; WordPress</title>
 	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
 <link rel="dns-prefetch" href="//s.w.org">
-<link rel="stylesheet" id="dashicons-css" href="/wp-includes/css/dashicons.min.css?ver=5.8.1" type="text/css" media="all">
-<link rel="stylesheet" id="buttons-css" href="/wp-includes/css/buttons.min.css?ver=5.8.1" type="text/css" media="all">
-<link rel="stylesheet" id="forms-css" href="/wp-admin/css/forms.min.css?ver=5.8.1" type="text/css" media="all">
-<link rel="stylesheet" id="l10n-css" href="/wp-admin/css/l10n.min.css?ver=5.8.1" type="text/css" media="all">
-<link rel="stylesheet" id="login-css" href="/wp-admin/css/login.min.css?ver=5.8.1" type="text/css" media="all">
+<link rel="stylesheet" id="dashicons-css" href="https://urban-tlemcen.netlify.app/wp-includes/css/dashicons.min.css?ver=5.8.1" type="text/css" media="all">
+<link rel="stylesheet" id="buttons-css" href="https://urban-tlemcen.netlify.app/wp-includes/css/buttons.min.css?ver=5.8.1" type="text/css" media="all">
+<link rel="stylesheet" id="forms-css" href="https://urban-tlemcen.netlify.app/wp-admin/css/forms.min.css?ver=5.8.1" type="text/css" media="all">
+<link rel="stylesheet" id="l10n-css" href="https://urban-tlemcen.netlify.app/wp-admin/css/l10n.min.css?ver=5.8.1" type="text/css" media="all">
+<link rel="stylesheet" id="login-css" href="https://urban-tlemcen.netlify.app/wp-admin/css/login.min.css?ver=5.8.1" type="text/css" media="all">
 	<meta name="referrer" content="strict-origin-when-cross-origin">
 		<meta name="viewport" content="width=device-width">
 	<style type="text/css">.login label{display:block;}#registerform #wp-submit{float:none;margin-top:15px;}.login.clc-text-logo:not(.clc-both-logo) h1 a{ background-image: none !important;text-indent: unset;width:auto !important;height: auto !important; }#login form p label br{display:none}body:not( .ml-half-screen ) .ml-form-container{background:transparent !important;}.login:not(.clc-both-logo) h1 a{background-position: center;background-size:contain !important;}.ml-container #login{ position:relative;padding: 0;width:100%;max-width:320px;margin:0;}#loginform,#registerform,#lostpasswordform{box-sizing: border-box;max-height: 100%;background-position: center;background-repeat: no-repeat;background-size: cover;}.ml-container{position:relative;min-height:100vh;display:flex;height:100%;min-width:100%;}.ml-container .ml-extra-div{background-position:center;background-size:cover;background-repeat:no-repeat}body .ml-form-container{display:flex;align-items:center;justify-content:center}body:not( .ml-half-screen ) .ml-container .ml-extra-div{position:absolute;top:0;left:0;width:100%;height:100%}body:not( .ml-half-screen ) .ml-container .ml-form-container{width:100%;min-height:100vh}body.ml-half-screen .ml-container{flex-wrap:wrap}body.ml-half-screen .ml-container>.ml-extra-div,body.ml-half-screen .ml-container>.ml-form-container{width:50%}body.ml-half-screen.ml-login-align-2 .ml-container>div,body.ml-half-screen.ml-login-align-4 .ml-container>div{width:100%;flex-basis:50%;}body.ml-half-screen.ml-login-align-2 .ml-container{flex-direction:column-reverse}body.ml-half-screen.ml-login-align-4 .ml-container{flex-direction:column}body.ml-half-screen.ml-login-align-1 .ml-container{flex-direction:row-reverse}body.ml-login-vertical-align-1 .ml-form-container{align-items:flex-start}body.ml-login-vertical-align-3 .ml-form-container{align-items:flex-end}body.ml-login-horizontal-align-1 .ml-form-container{justify-content:flex-start}body.ml-login-horizontal-align-3 .ml-form-container{justify-content:flex-end}@media only screen and (max-width: 768px) {body.ml-half-screen .ml-container > .ml-extra-div, body.ml-half-screen .ml-container > .ml-form-container{width:50% !important;}}.login input[type=text]:focus, .login input[type=search]:focus, .login input[type=radio]:focus, .login input[type=tel]:focus, .login input[type=time]:focus, .login input[type=url]:focus, .login input[type=week]:focus, .login input[type=password]:focus, .login input[type=checkbox]:focus, .login input[type=color]:focus, .login input[type=date]:focus, .login input[type=datetime]:focus, .login input[type=datetime-local]:focus, .login input[type=email]:focus, .login input[type=month]:focus, .login input[type=number]:focus, .login select:focus, .login textarea:focus{ box-shadow: none; }@media only screen and (max-width: 577px){body.ml-half-screen .ml-container > .ml-extra-div, body.ml-half-screen .ml-container > .ml-form-container{width:100% !important;}body.ml-half-screen.ml-login-align-1 .ml-container .ml-extra-div, body.ml-half-screen.ml-login-align-1 .ml-container .ml-form-container,body.ml-half-screen.ml-login-align-3 .ml-container .ml-extra-div,body.ml-half-screen.ml-login-align-3 .ml-container .ml-form-container{ width: 100%; }body.ml-half-screen.ml-login-align-1 .ml-container .ml-extra-div,body.ml-half-screen.ml-login-align-3 .ml-container .ml-extra-div{position: absolute;top: 0;left: 0;width: 100%;height: 100%;}}</style>
 <style type="text/css" id="clc-style">.ml-container .ml-extra-div{
-background-image:url(/wp-content/uploads/2021/10/800px-Le_mausolee_de_Sidi_Boumediene_tlemcen_3.jpg);
+background-image:url(https://urban-tlemcen.netlify.app/wp-content/uploads/2021/10/800px-Le_mausolee_de_Sidi_Boumediene_tlemcen_3.jpg);
 background-color:rgba(247, 247, 247, 1);
 }
 .login:not(.clc-both-logo) h1 a{
@@ -73,7 +73,7 @@ color:rgba(175, 130, 100, 1);
 display:none;
 }</style>
 <style type="text/css" id="clc-columns-style">.ml-half-screen.ml-login-align-3 .ml-container .ml-extra-div,.ml-half-screen.ml-login-align-1 .ml-container .ml-form-container{ width:66.666666666667%; }.ml-half-screen.ml-login-align-4 .ml-container .ml-extra-div,.ml-half-screen.ml-login-align-2 .ml-container .ml-form-container{ flex-basis:66.666666666667%; }.ml-half-screen.ml-login-align-3 .ml-container .ml-form-container,.ml-half-screen.ml-login-align-1 .ml-container .ml-extra-div{ width:33.333333333333%; }.ml-half-screen.ml-login-align-4 .ml-container .ml-form-container,.ml-half-screen.ml-login-align-2 .ml-container .ml-extra-div{ flex-basis:33.333333333333%; }</style>
-<style type="text/css" id="clc-logo-style">.login.clc-both-logo h1 a{width:100%;height:100%;text-indent: unset;background-position:top center !important;padding-top:30px; background-size: 20px 20px; margin-top: -15px; position:relative;background-image:url(/wp-admin/images/wordpress-logo.svg)}</style>
+<style type="text/css" id="clc-logo-style">.login.clc-both-logo h1 a{width:100%;height:100%;text-indent: unset;background-position:top center !important;padding-top:30px; background-size: 20px 20px; margin-top: -15px; position:relative;background-image:url(https://urban-tlemcen.netlify.app/wp-admin/images/wordpress-logo.svg)}</style>
 <style type="text/css" id="clc-custom-css">.ml-form-container .submit input[type='submit'] {width: 100%; box-sizing: border-box;display: inline-block;text-align: center;border-radius: 30px;padding: 0 20px;height: 38px;line-height: 1;font-weight: bold;vertical-align: middle;box-shadow:none; }.ml-form-container input[type='password'],.ml-form-container input[type='text']{box-shadow:none;}</style>
 <style type="text/css" id="clc-custom-background-link">body .ml-container .ml-extra-div .clc-custom-background-link {display:block; width:100%; height:100%;}</style>	</head>
 	<body class="login no-js login-action-login wp-core-ui  locale-fr-fr ml-half-screen ml-login-align-3 ml-login-vertical-align-2 clc-text-logo">
@@ -83,9 +83,9 @@ display:none;
 	<div class="ml-container">
 <div class="ml-extra-div"></div>
 <div class="ml-form-container">	<div id="login">
-		<h1><a>Meber Only Social Network</a></h1>
+		<h1><a href="https://urban-tlemcen.netlify.app">Meber Only Social Network</a></h1>
 	
-		<form name="loginform" id="loginform" action="/wp-login.php" method="post">
+		<form name="loginform" id="loginform" action="https://urban-tlemcen.netlify.app/wp-login.php" method="post">
 			<p>
 				<label for="user_login">Username</label>
 				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off">
@@ -103,35 +103,35 @@ display:none;
 						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Se souvenir de moi</label></p>
 			<p class="submit">
 				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Se connecter">
-									<input type="hidden" name="redirect_to" value="http://localhost/wordpress/wp-admin/">
+									<input type="hidden" name="redirect_to" value="https://urban-tlemcen.netlify.app/wp-admin/">
 									<input type="hidden" name="testcookie" value="1">
 			</p>
 		</form>
 
 					<p id="nav">
-								<a href="/wp-login.php?action=lostpassword">Mot de passe oublié ?</a>
+								<a href="https://urban-tlemcen.netlify.app/wp-login.php?action=lostpassword">Mot de passe oublié ?</a>
 			</p>
 					<script type="text/javascript">
 			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
 				<p id="backtoblog">
-			<a href="/">&larr; Aller sur Urban Tlemcen</a>		</p>
+			<a href="https://urban-tlemcen.netlify.app/">&larr; Aller sur Urban Tlemcen</a>		</p>
 			</div>
 	</div>
 </div>
-<script type="text/javascript" src="/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
-<script type="text/javascript" src="/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
+<script type="text/javascript" src="https://urban-tlemcen.netlify.app/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
+<script type="text/javascript" src="https://urban-tlemcen.netlify.app/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
 <script type="text/javascript" id="zxcvbn-async-js-extra">
 /* <![CDATA[ */
-var _zxcvbnSettings = {"src":"http:\/\/localhost\/wordpress\/wp-includes\/js\/zxcvbn.min.js"};
+var _zxcvbnSettings = {"src":"https:\/\/urban-tlemcen.netlify.app\/wp-includes\/js\/zxcvbn.min.js"};
 /* ]]> */
 </script>
-<script type="text/javascript" src="/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
-<script type="text/javascript" src="/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.7" id="regenerator-runtime-js"></script>
-<script type="text/javascript" src="/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
-<script type="text/javascript" src="/wp-includes/js/dist/hooks.min.js?ver=a7edae857aab69d69fa10d5aef23a5de" id="wp-hooks-js"></script>
-<script type="text/javascript" src="/wp-includes/js/dist/i18n.min.js?ver=5f1269854226b4dd90450db411a12b79" id="wp-i18n-js"></script>
+<script type="text/javascript" src="https://urban-tlemcen.netlify.app/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script type="text/javascript" src="https://urban-tlemcen.netlify.app/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.7" id="regenerator-runtime-js"></script>
+<script type="text/javascript" src="https://urban-tlemcen.netlify.app/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
+<script type="text/javascript" src="https://urban-tlemcen.netlify.app/wp-includes/js/dist/hooks.min.js?ver=a7edae857aab69d69fa10d5aef23a5de" id="wp-hooks-js"></script>
+<script type="text/javascript" src="https://urban-tlemcen.netlify.app/wp-includes/js/dist/i18n.min.js?ver=5f1269854226b4dd90450db411a12b79" id="wp-i18n-js"></script>
 <script type="text/javascript" id="wp-i18n-js-after">
 wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 </script>
@@ -147,14 +147,14 @@ var pwsL10n = {"unknown":"Force du mot de passe inconnue.","short":"Très faible
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", {"translation-revision-date":"2021-10-20 22:02:44+0000","generator":"GlotPress\/3.0.0-alpha.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n > 1;","lang":"fr"},"%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code.":["%1$s est obsolète depuis la version %2$s ! Utilisez %3$s à la place. Pensons à écrire du code plus inclusif."]}},"comment":{"reference":"wp-admin\/js\/password-strength-meter.js"}} );
 </script>
-<script type="text/javascript" src="/wp-admin/js/password-strength-meter.min.js?ver=5.8.1" id="password-strength-meter-js"></script>
-<script type="text/javascript" src="/wp-includes/js/underscore.min.js?ver=1.13.1" id="underscore-js"></script>
+<script type="text/javascript" src="https://urban-tlemcen.netlify.app/wp-admin/js/password-strength-meter.min.js?ver=5.8.1" id="password-strength-meter-js"></script>
+<script type="text/javascript" src="https://urban-tlemcen.netlify.app/wp-includes/js/underscore.min.js?ver=1.13.1" id="underscore-js"></script>
 <script type="text/javascript" id="wp-util-js-extra">
 /* <![CDATA[ */
 var _wpUtilSettings = {"ajax":{"url":"\/wordpress\/wp-admin\/admin-ajax.php"}};
 /* ]]> */
 </script>
-<script type="text/javascript" src="/wp-includes/js/wp-util.min.js?ver=5.8.1" id="wp-util-js"></script>
+<script type="text/javascript" src="https://urban-tlemcen.netlify.app/wp-includes/js/wp-util.min.js?ver=5.8.1" id="wp-util-js"></script>
 <script type="text/javascript" id="user-profile-js-extra">
 /* <![CDATA[ */
 var userProfileL10n = {"user_id":"0","nonce":"c6320068e0"};
@@ -167,7 +167,7 @@ var userProfileL10n = {"user_id":"0","nonce":"c6320068e0"};
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", {"translation-revision-date":"2021-10-20 22:02:44+0000","generator":"GlotPress\/3.0.0-alpha.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n > 1;","lang":"fr"},"Your new password has not been saved.":["Votre nouveau mot de passe n’a pas été enregistré."],"Hide":["Masquer"],"Show":["Afficher"],"Confirm use of weak password":["Confirmer l’utilisation du mot de passe faible"],"Hide password":["Cacher le mot de passe"],"Show password":["Afficher le mot de passe"]}},"comment":{"reference":"wp-admin\/js\/user-profile.js"}} );
 </script>
-<script type="text/javascript" src="/wp-admin/js/user-profile.min.js?ver=5.8.1" id="user-profile-js"></script>
+<script type="text/javascript" src="https://urban-tlemcen.netlify.app/wp-admin/js/user-profile.min.js?ver=5.8.1" id="user-profile-js"></script>
 		<script>
 		/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
 		</script>
